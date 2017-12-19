5b85ae5953716459eb81aaf8725dba6f2c9e46b0
Syping <schiedelrafael@keppe.org>
2017-12-19 10:19:36 +0100

    travis build script update

--- travis/win32build.sh
+++ travis/win32build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-QMAKE=/home/windev/opt/qt32s/bin/qmake
+QMAKE=qmake
 mkdir -p build && mkdir -p assets && cd build && $QMAKE LIBS+=-static ../ && make -j4 && cd release && cp -Rf *.exe ../../assets/ && cd ../../assets/ && upx *.exe
