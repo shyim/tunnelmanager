091108888f5e7822d9922c1b008fe0562b0323ab
Syping <schiedelrafael@keppe.org>
2020-10-07 14:46:23 +0200

    Fix docker image location

--- travis/win64docker.sh
+++ travis/win64docker.sh
@@ -2,7 +2,7 @@
 
 export QT_VERSION=5.15.x
 PROJECT_DIR=$(pwd)
-DOCKER_IMAGE=syping/qt5-llvm-mingw-docker:$QT_VERSION
+DOCKER_IMAGE=syping/qt5-static-llvm-mingw:${QT_VERSION}
 docker pull $DOCKER_IMAGE
 docker run --rm \
 	-v "${PROJECT_DIR}:/root/tunnelmanager/" \
